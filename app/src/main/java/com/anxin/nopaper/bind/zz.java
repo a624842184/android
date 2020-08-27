852a88642ffdc2af18df06f0d3761efb9f80bdc6
a624842184 <38685476+a624842184@users.noreply.github.com>
2020-08-27 10:59:25 +0800

    Add files via upload
    test

---
+++ app/src/main/java/com/anxin/nopaper/bind/zz.java
@@ -0,0 +1,14 @@
+package com.anxin.nopaper.bind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class zz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zz2);
+    }
+}
